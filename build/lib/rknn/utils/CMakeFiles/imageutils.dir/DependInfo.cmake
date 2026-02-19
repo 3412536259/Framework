809090cd4716ba370112bfde5f11a506d1612c5c
kdid237 <zhutaile0928@foxmail.com>
2026-02-19 17:16:39 +0800

    Revert "命令服务头文件1"
    This reverts commit 156deccb2639ef6fce741f3bc5b8382387c8ffb6.

---
+++ build/lib/rknn/utils/CMakeFiles/imageutils.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lin/Desktop/Framework/lib/rknn/utils/image_utils.c" "/home/lin/Desktop/Framework/build/lib/rknn/utils/CMakeFiles/imageutils.dir/image_utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DISABLE_RGA"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/rknn/utils"
+  "../lib/rknn/3rdparty/stb_image"
+  "../lib/rknn/3rdparty/librga/include"
+  "../lib/rknn/3rdparty/jpeg_turbo/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
